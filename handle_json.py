03c8ec565327b7c188d5564223099c3252cdde34
kieulytt <kieulydt@gmail.com>
2020-07-11 23:14:56 +0700

    issue1 post ne

--- handle_json.py
+++ handle_json.py
@@ -1,10 +1,18 @@
 import json
+
 f = open('gsmarena.json')
 data = json.load(f)
 f.close()
 
-list_key = ['brand', 'name', 'models', 'color', 'ram', 'storage', 'rom', 'screen', 'screen_size', 'sim', 'os', 'behind_camera', 'front_camera', 'url', 'error_url']
-new_dict={}
+list_key = ['brand', 'name', 'models', 'color', 'ram', 'storage', 'rom', 'screen', 'screen_size', 'sim', 'os',
+            'behind_camera', 'front_camera', 'url', 'error_url']
+
+
+def testthoi():
+    pass
+
+
+new_dict = {}
 for ele in list_key:
     new_dict[ele] = '-1'
 for items in data:
